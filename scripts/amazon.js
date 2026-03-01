4de8609ec0f7b8a97a3494d9b9196ebf018fc11b
Kala <melonmalow@gmail.com>
2026-03-01 12:49:48 +0800

    added quanity-selector interaction on js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class ="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -61,25 +61,26 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
    const productId = button.dataset.productId;
-   
+    let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
     let matchingItem;
 
     cart.forEach((item) => {
        if (productId === item.productId) {
           matchingItem = item;
-           }
-     });
+        };
+    });
    
-       if (matchingItem) {
-        matchingItem.quantity +=1;
-        } else {
+      if (matchingItem) {
+        matchingItem.quantity +=Number(quantitySelector.value);
+      } else {
         cart.push({
-        productId: productId,
-        quantity: 1
+          productId: productId,
+          quantity: Number(quantitySelector.value)
         });
-        }
+      };
       
         let cartQuantity = 0;
+        
         cart.forEach((item) => {
           cartQuantity += item.quantity;
         });
