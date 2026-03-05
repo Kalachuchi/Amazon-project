1a72158412328fab35c86e2d39a4face5b592dab
Kala <melonmalow@gmail.com>
2026-03-06 03:38:06 +0800

    Rename tests-jasmine folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,5 +1,20 @@
-import { formatCurrency } from '../scripts/utils/money.js';
+import { formatCurrency } from '../../scripts/utils/money.js';
 
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars',() =>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+  it('works with 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+  it('Rounds up to the nearest cent',()=> {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+});
+
+
+/*
 console.log('test suite: formatCurrency')
 
 console.log('converts cents into dollars');
@@ -32,4 +47,4 @@ if (formatCurrency(2000.4) === '20.00') {
     console.log('passed');
 } else {
   console.log('failed');
-}
+} */
